c034da80dd2b52172e196a77eaf9da4e5854d9ea
Marcus Jensen <marcusjensen1515@gmail.com>
2020-05-23 13:39:54 -0700

    basic header looking good

--- src/Header/header.js
+++ src/Header/header.js
@@ -9,7 +9,7 @@ export default function Header(){
 
   return(
   <>
-    <h1 className="welcomeHeader">Welcome to NimbleNote</h1>
+    <h1 className="welcomeHeader">Welcome to NimbleNote!</h1>
     <h4 className="appDescription">A convenient and fast way to take down your brilliant thoughts and ideas!</h4>
   </>
   )
