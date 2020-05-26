7d80298d37c9b54f5ba5dfc8c49ca51a04994855
Marcus Jensen <marcusjensen1515@gmail.com>
2020-05-26 14:02:56 -0700

    created instructions component

---
+++ src/Instructions/instructions.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css'
+
+
+
+export default function Header(){
+
+
+
+  return(
+  <>
+    <ul>
+      <li>Click "New Note" to create a new note</li>
+    </ul>
+  </>
+  )
+}
