c63c62cbd1f9b8c2e71f99cb3410e75f1618d219
Marcus Jensen <marcusjensen1515@gmail.com>
2020-05-26 14:06:27 -0700

    adding content to instructions static component

--- src/Instructions/instructions.js
+++ src/Instructions/instructions.js
@@ -11,6 +11,8 @@ export default function Header(){
   <>
     <ul>
       <li>Click "New Note" to create a new note</li>
+      <li>To edit a note, click on it. You can edit the title of the note and the body of the note by modifying the presented text</li>
+      <li>NimbleNote Automatically saves your work after you stop typing for 1.5 seconds.</li>
     </ul>
   </>
   )
