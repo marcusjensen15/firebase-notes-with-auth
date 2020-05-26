0f993de8f593e4dbbdaad6f24e2c4d6df7efd7b3
Marcus Jensen <marcusjensen1515@gmail.com>
2020-05-26 14:37:04 -0700

    complete content in Instructions component

--- src/Instructions/instructions.js
+++ src/Instructions/instructions.js
@@ -8,15 +8,17 @@ export default function InstructionsComponent(){
 
 
   return(
-  <>
-    <h2 className="instructionTitle"> Welcome to NimbleNote!</h2>
-    <ul>
+  <div className="instructionsContainer">
+    <h2>Welcome to NimbleNote!</h2>
+
+    <ul className="instructionsList">
       <li>Click "New Note" to create a new note</li>
       <li>To edit a note, click on the note title in the left hand column. You can edit the title of the note and the body of the note by modifying the presented text</li>
       <li>NimbleNote Automatically saves your work after you stop typing for 1.5 seconds. There is no 'save' button.</li>
       <li>By clicking the trash can, you will delete your note.</li>
     </ul>
     <h2>Enjoy!</h2>
-  </>
+
+  </div>
   )
 }
