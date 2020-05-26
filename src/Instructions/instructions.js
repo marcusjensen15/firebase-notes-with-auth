57eed35d79c56af809d1ab469d5fc56ad1103d5e
Marcus Jensen <marcusjensen1515@gmail.com>
2020-05-26 14:44:16 -0700

    adjust punctionation on instructions component

--- src/Instructions/instructions.js
+++ src/Instructions/instructions.js
@@ -9,15 +9,15 @@ export default function InstructionsComponent(){
 
   return(
   <div className="instructionsContainer">
-    <h2>Welcome to NimbleNote!</h2>
+    <h2 className="instructionsTitle">Welcome to NimbleNote!</h2>
 
     <ul className="instructionsList">
-      <li>Click "New Note" to create a new note</li>
-      <li>To edit a note, click on the note title in the left hand column. You can edit the title of the note and the body of the note by modifying the presented text</li>
-      <li>NimbleNote Automatically saves your work after you stop typing for 1.5 seconds. There is no 'save' button.</li>
+      <li>Click "New Note" to create a new note.</li>
+      <li>To edit a note, click on the note title in the left hand column. You can edit the title of the note and the body of the note by modifying the presented text.</li>
+      <li>NimbleNote Automatically saves your work after you stop typing for 1.5 seconds. There is no 'Save' button.</li>
       <li>By clicking the trash can, you will delete your note.</li>
     </ul>
-    <h2>Enjoy!</h2>
+    <h2 className="instructionsTitle">Enjoy!</h2>
 
   </div>
   )
