c642b4bca34d716bf2183b3662c90e5a36ce738a
Marcus Jensen <marcusjensen1515@gmail.com>
2020-05-26 09:33:48 -0700

    change width back, creating new branch

--- src/signup/styles.js
+++ src/signup/styles.js
@@ -4,8 +4,8 @@ const styles = theme => ({
     display: 'block', // Fix IE 11 issue.
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(500 + theme.spacing.unit * 3 * 2)]: {
-      width: 500,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
       marginLeft: 'auto',
       marginRight: 'auto',
     },
